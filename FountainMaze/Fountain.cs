de609cb9fffc6b7ab3ec9960d020a848669f4e92
hirgin <97668700+hirgin@users.noreply.github.com>
2023-07-20 03:01:13 -0700

    Reworked Entirety of Fountain.cs
    Restarted Fountain game in order to create better class synergy across entire program in order to further expand game later.

--- FountainMaze/Fountain.cs
+++ FountainMaze/Fountain.cs
@@ -1,132 +1,98 @@
-
-Game game = new Game();
-game.run();
-
-
-
-
-class Game // Class that holds game logic 
+Game start = new Game();
+start.initGame(8,8);
+class Map
 {
+    private roomType[,] _map;
+    public int _col { get; }
+    public int _row { get; }
     
-    public Grid gameGrid { get; }
-    public Fountain fountain { get; }
-    public Player player { get; }
 
-    public Game()
+    public Map(int row, int col)
     {
-        gameGrid = new Grid();
-        fountain = new Fountain();
-        player = new Player();
-    }
-
-    public void checkRoom()
-    {
-        if (gameGrid._grid[player.x, player.y] == "fountain")
+        _col = col;
+        _row = row;
+        
+        _map = new roomType[_row, _col]; // initialize map
+        changeRoom(0, 0, roomType.Entrance);
+        setRooms();
+        for (int i = 0; i < _row; i++)
         {
-            if (fountain.onState) { Console.WriteLine("You hear the rushing waters from the Fountain of Objects. It has been reactivated!"); }
-            else { Console.WriteLine("You hear water drippin in this room. The Fountain of Objects is here!"); }
-        }
-        if (gameGrid._grid[player.x, player.y] == "entrance")
-        {
-            Console.WriteLine("You see light in this room coming from outside the cavern. This is the entrance.");
-        }
-    }
-    public bool gameEnd()
-    {
-        if (fountain.onState && player.x == 0 && player.y == 0)
-        {
-            Console.WriteLine("The Fountain of Objects has been reactivated, and you have escaped with your life!\nYou win!");
-            return true;
-        }
-        else return false;
-    }
-
-    public void displayPlayer()
-    {
-        Console.WriteLine($"You are in the room at (Row = {player.x}, Column = {player.y})");
-    }
-
-    public void userChoice()
-    {
-        Console.Write("What do you want to do? ");
-        string userInput = Console.ReadLine();
-        if (userInput == "move east") { player.movePlayer("east"); return; }
-        if (userInput == "move west") { player.movePlayer("west"); return; }
-        if (userInput == "move north") { player.movePlayer("north"); return; }
-        if (userInput == "move south") { player.movePlayer("south"); return; }
-        if (userInput == "enable fountain" && player.x == 0 && player.y == 2) { fountain.enableFountain(); return; }
-
-    }
-
-    public void run()
-    {
-        while (!gameEnd())
-        {
-            Console.WriteLine("----------------------------------------------------------------------------------");
-            displayPlayer();
-            checkRoom();
-            userChoice();
-            gameEnd();
-        }
-    }
-}
-
-class GameObject // Generic class for all game objects to have
-{
-    public int x { get; protected set; }
-    public int y { get; protected set; }
-    public static bool isOOB(int x, int y) => (x < 0 || y < 0 || x > 3 || y > 3) ? false : true; // if it is out of bounds, return false, else return true
-
-}
-
-class Fountain : GameObject
-{
-    public Fountain()
-    {
-        x = 0; y = 2;
-    }
-    public bool onState { get; private set; } = false;
-
-    public void enableFountain()
-    {
-        onState = true;
-    }
-}
-class Player : GameObject
-{
-   public Player() 
-    {
-        x = 0; 
-        y = 0;    
-    }
-    public void movePlayer(string direction)
-    {
-        if (direction == "north" && isOOB(x + 1, y) != false) { x += 1; }
-        else if(direction == "south" && isOOB(x - 1, y) != false){ x -= 1; }
-        else if (direction == "west" && isOOB(x, y - 1) != false) { y -= 1; }
-        else if (direction == "east" && isOOB(x, y + 1) != false) { y +=1; }
-    }
-
-    
-}
-
-class Grid
-{
-    public string[,] _grid = new string[4,4];
-
-    public Grid() 
-    {
-        for(int i = 0; i < 4; i++)
-        {
-            for(int j = 0; j < 4; j++)
+            for (int j = 0; j < _col; j++)
             {
-                _grid[i, j] = "empty";
+                Console.Write($"{_map[i, j]} ");
+            }
+            Console.WriteLine();
+        }
+    }
+
+    private void setRooms()
+    {
+        if (_col == 4) { changeRoom(rGen.genRandom(_col), rGen.genRandom(_col), roomType.Pits); } // create 1 pit randomly in map
+
+        if (_col == 6) // create 2 pits randomly in map
+        {
+            for (int i = 0; i < 2; i++)
+            {
+                changeRoom(rGen.genRandom(_col), rGen.genRandom(_col), roomType.Pits);
+            }
+        } 
+
+        if (_col == 8) // create 4 pits randomly in map
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                changeRoom(rGen.genRandom(_col), rGen.genRandom(_col), roomType.Pits);
+
             }
         }
-        _grid[0, 0] = "entrance";
-        _grid[0, 2] = "fountain";
+
+        changeRoom(rGen.genRandom(_col), rGen.genRandom(_col), roomType.Fountain);
+
     }
 
 
+    public bool inBounds(int x, int y) // function checks if spot is legal and in bounds
+    {
+        if (x < 0 || y < 0 || x > _row || y > _col)
+        {
+           
+            return false;
+        }
+        return true;
+    }
+
+    public void changeRoom(int x, int y, roomType room) // check if room is in bounds and change type
+    {
+        if (inBounds(x,y))
+        {
+            _map[x,y] = room;
+        }
+    }
 
 }
+
+class rGen
+{
+    public static int genRandom(int size) // random number generator
+    {
+        var random = new Random();
+        int randomNumber = random.Next(0, size);
+        return randomNumber;
+    }
+    
+  
+}
+class Game
+{
+    public Map map;
+
+    public string mapSize { get; private set; }
+    public void initGame(int row, int col)
+    {
+        map = new Map(row, col);
+        
+    }
+}
+public record Coordinates(int col, int row);
+
+enum roomType { Empty, Fountain, Pits, Maelstrom, Entrance, OOB }
